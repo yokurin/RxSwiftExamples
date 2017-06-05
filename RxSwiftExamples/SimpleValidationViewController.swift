b9b541865a406fc6571902a4f3f762d310f132e7
林　翼 <tsubasa_hayashi@r.recruit.co.jp>
2017-06-05 19:28:14 +0900

    [WIP] SimpleValidation

---
+++ RxSwiftExamples/SimpleValidationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleValidationViewController.swift
+//  RxSwiftExamples
+//
+//  Created by 林　翼 on 2017/06/05.
+//  Copyright © 2017年 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class SimpleValidationViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
